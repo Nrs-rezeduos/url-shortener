951af2534e42fc67b8db92d32ff2b40b32a5c43f
Nrs-rezeduos <137420858+Nrs-rezeduos@users.noreply.github.com>
2026-03-09 11:07:19 +0530

    backend on url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/url-shortener/',
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': 'http://localhost:3002'
-    }
+   proxy: {
+  '/api': {
+    target: 'https://url-shortener-1egv.onrender.com',
+    changeOrigin: true,
+    secure: true
+  }
+}
   }
 })
